0348da95f321bc03d396721bf32be3cc369e69b6
Bruno Beserra <bruno.n.beserra@gmail.com>
2024-05-18 11:44:49 -0700

    Lecture 2

---
+++ CSIS2175/Rental.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Rental {
+    public static final double hour_minute = 60.0;
+    public static final double rental_rate = 40.0;
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        int firstNumber = 10;
+        int secondNumber = 20;
+        float thirdNumber = 34.5f;
+        float result = firstNumber + secondNumber + thirdNumber;
+
+
+        System.out.println("The result value is: " + (int)(result));
+
+        int condition = 5;
+
+        String sum = condition>5?"I am ok":"I am not ok";
+
+
+    }
+    
+}
